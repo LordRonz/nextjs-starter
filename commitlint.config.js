f97c78ed905d4c3fc52eb152f662069adc369e05
LordRonz <aarontanhar2000@gmail.com>
2022-01-06 17:11:31 +0700

    feat: dependabot update github actions

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    //   TODO Add Scope Enum Here
+    // TODO Add Scope Enum Here
     // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
     'type-enum': [
       2,
       'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'chore',
-        'style',
-        'refactor',
-        'ci',
-        'test',
-        'revert',
-        'perf',
-        'vercel',
-      ],
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'vercel'],
     ],
   },
 };
